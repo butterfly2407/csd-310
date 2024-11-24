147bd5914ad6b9452c73c42f104e322659e817ea
katieh <katiehilliard24@gmail.com>
2024-11-24 15:05:55 -0500

    Add mysql_test.py for Module 6 assignment

---
+++ module-6/mysql_test.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host='LAPTOP-ED96K4VM',
+        database='movies',
+        user='khilliard24',
+        password='Stanle@3!',
+    )
+
+    if connection.is_connected():
+        db_info = connection.get_server_info()
+        print("Successfully connected to MySQL database")
+        print(f"MySQL Server version: {db_info}")
+
+except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed")
